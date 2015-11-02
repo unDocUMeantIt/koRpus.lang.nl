103fec10b210492a6512e945ca2d5de0d9d80126
unDocUMeantIt <m@reaktanz.de>
2015-11-02 20:38:44 +0100

    initial commit

---
+++ R/koRpus.lang.nl-package.R
@@ -0,0 +1,26 @@
+#' Language support for koRpus: Dutch.
+#'
+#' \tabular{ll}{
+#' Package: \tab koRpus.lang.nl\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.01-2\cr
+#' Date: \tab 2015-11-02\cr
+#' Depends: \tab R (>= 2.10.0),koRpus (>= 0.06-3), methods\cr
+#' Encoding: \tab UTF-8\cr
+#' License: \tab GPL (>= 3)\cr
+#' LazyLoad: \tab yes\cr
+#' URL: \tab http://reaktanz.de/?c=hacking&s=koRpus\cr
+#' }
+#'
+#' Adds support for the Dutch language to the koRpus package.
+#' Due to some restrictions on CRAN, the full package sources are only available from the project homepage.
+#' To ask for help, report bugs, suggest feature improvements, or discuss the global development of the package,
+#' please subscribe to the koRpus-dev mailing list: https://ml06.ispgateway.de/mailman/listinfo/korpus-dev_r.reaktanz.de
+#'
+#' @aliases koRpus.lang.nl-package
+#' @name koRpus.lang.nl-package
+#' @docType package
+#' @title The koRpus.lang.nl Package
+#' @author m.eik michalke, with contributions from Christophe Bechet
+#' @keywords package
+NULL
---
+++ R/lang.support-nl.R
@@ -0,0 +1,186 @@
+# Copyright 2010-2015 Meik Michalke <meik.michalke@hhu.de>
+#
+# This file is part of the R package koRpus.lang.nl.
+#
+# koRpus.lang.nl is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# koRpus.lang.nl is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with koRpus.lang.nl.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# this script is providing additional support for language "nl".
+# adjustments kindly provided by Christophe Bechet
+
+#' Language support for Dutch
+#' 
+#' This function adds support for Dutch to the koRpus package. You should not
+#' need to call it manually, as that is done automatically when this package is
+#' loaded.
+#' 
+#' In particular, this function adds the following:
+#' \itemize{
+#'  \item \code{lang}: The additional language "nl" to be used with koRpus
+#'  \item \code{hyphen}: An additional set of hyphenation patterns (see \code{\link{hyph.nl}})
+#'  \item \code{treetag}: The additional preset "nl-utf8", implemented according to the respective
+#'    TreeTagger[1] script
+#'  \item \code{POS tags}: An additional set of tags, implemented using the documentation for the corresponding
+#'    TreeTagger parameter set[2]
+#' }
+#'
+#' @references
+#' [1] \url{http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/}
+#'
+#' [2] \url{http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/dutch-tagset.txt}
+#' @export
+lang.support.nl <- function() {
+
+  # first tell koRpus where to find hyphenation patterns (see ?set.lang.support for details)
+  koRpus::set.lang.support(target="hyphen",
+    value=list(
+      "nl"=c("nl", package="koRpus.lang.nl")
+    )
+  )
+
+
+  # here you have to adjust the parameters according to the contents of the TreeTagger
+  # scripts for your language (see ?set.lang.support for details)
+  #  - if there's both UTF-8 and Latin1 scripts, add them both (as "nl-utf8" and "nl")
+  #  - add both the unix and windows equivalents
+  #  - if some setting is missing, just set it to an empty vector (c())
+  koRpus::set.lang.support(target="treetag",
+    value=list(
+      "nl-utf8"=list(
+        ## preset: "nl-utf8"
+        # tags "utf-8" encoded text files
+        lang      = "nl",
+        encoding  = "UTF-8",
+        preset    = function(TT.cmd, TT.bin, TT.lib, unix.OS){
+          if(isTRUE(unix.OS)){
+            # preset for unix systems
+
+            # note: these objects are set here for convenience, the
+            # actual important part is the return value below
+            TT.abbrev   <- file.path(TT.lib, "dutch-abbreviations")
+            return(
+              list(
+                # you should change these according to the TreeTagger script
+                TT.tokenizer        = file.path(TT.cmd, "utf8-tokenize.perl"),
+                TT.tagger           = file.path(TT.bin, "tree-tagger"),
+                TT.abbrev           = TT.abbrev,
+                TT.params           = file.path(TT.lib, "dutch-utf8.par"),
+
+                TT.tknz.opts.def    = c(),
+                TT.tknz.opts        = paste("-a", TT.abbrev),
+                TT.lookup.command   = c(),
+                TT.filter.command   = c()
+              )
+            )
+          } else {
+            # preset for windows systems
+            TT.abbrev   <- file.path(TT.lib, "dutch-abbreviations")
+            return(
+              list(
+                TT.tokenizer        = file.path(TT.cmd, "utf8-tokenize.perl"),
+                TT.tagger           = file.path(TT.bin, "tree-tagger.exe"),
+                TT.abbrev           = TT.abbrev,
+                TT.params           = file.path(TT.lib, "dutch-utf8.par"),
+
+                TT.tknz.opts.def    = c(),
+                TT.tknz.opts        = paste("-a", TT.abbrev),
+                TT.lookup.command   = c(),
+                TT.filter.command   = c()
+              )
+            )
+          }
+        }
+      )
+    )
+  )
+
+
+  # finally, add the POS tagset information (see ?set.lang.support for details)
+  # the list is split into three parts, to be able to distinct between
+  # words (including numbers etc.), normal punctuation and sentence ending punctuation.
+  # this is mainly used for filtering purposes and statistics.
+  #
+  # note that each tag must be defined by three values:
+  #   - the original TreeTagger abbreviation
+  #   - a global "word class" definition like "noun", "verb" etc.
+  #   - a human readable explaination of the abbreviation
+  koRpus::set.lang.support(target="kRp.POS.tags",
+    ## tag and class definitions
+    # nl -- Dutch
+    # see <http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/data/dutch-tagset.txt>, but also 
+    # see <https://www.sketchengine.co.uk/xdocumentation/wiki/tagsets/dutch_treetagger>
+    value=list(
+      "nl"=list(
+        tag.class.def.words=matrix(c(
+        # tag           class           meaning
+          "adj",        "adjective",    "Adjective",
+          "adj*kop",    "adjective",    "Truncated adjective",
+          "adjabbr",    "adjective",    "Abbreviated adjective",
+          "adv",        "adverb",       "Adverb",
+          "advabbr",    "adverb" ,      "Abbreviated adverb",
+          "conjcoord",  "conjunction",  "Coordinating conjunction",
+          "conjsubo",   "conjunction",  "Subordinating conjunction",
+          "det__art",   "determiner",   "Article",
+          "det__demo",  "determiner",   "Attributively used demonstrative pronoun",
+          "det__indef", "determiner",   "Attributively used indefinite pronoun",
+          "det__poss",  "determiner",   "Attributively used possessive pronoun",
+          "det__excl",  "determiner",   "Exclamative determiner", # (e.g. "zulk", "z'n")
+          "det__quest", "pronoun",      "Attributively used question pronoun",
+          "det__rel",   "pronoun",      "Attributively used relative pronoun",
+          "int",        "interjection", "Interjection",
+          "noun*kop",   "noun",         "Truncated noun",
+          "nounabbr",   "noun",         "Abbreviated noun",
+          "nounpl",     "noun",         "Plural noun",
+          "nounprop",   "noun",         "Proper name",
+          "nounsg",     "noun",         "Singular noun",
+          "num__card",  "number",       "Cardinal number",
+          "num__ord",   "number",       "Ordinal number",
+          "partte",     "particle",     "particle te",
+          "prep",       "preposition",  "Preposition",
+          "prepabbr",   "preposition",  "Abbreviated preposition",
+          "pronadv",    "adverb",       "Pronominal adverb",
+          "prondemo",   "pronoun",      "demonstrative pronoun used substitutively",
+          "pronindef",  "pronoun",      "Indefined pronoun",
+          "pronpers",   "pronoun",      "Personal pronoun",
+          "pronposs",   "pronoun",      "Possessive pronoun",
+          "pronquest",  "pronoun",      "Interrogative pronoun",
+          "pronrefl",   "pronoun",      "Reflexive pronoun",
+          "pronrel",    "pronoun",      "Relative prounoun",
+          "verbinf",    "verb",         "Infinitival verb",
+          "verbpapa",   "verb",         "Past participle verb",
+          "verbpastpl", "verb",         "Plural past tense verb",
+          "verbpastsg", "verb",         "Singular past tense verb",
+          "verbpresp",  "verb",         "present participle verb",
+          "verbprespl", "verb",         "Plural present tense verb",
+          "verbpressg", "verb",         "Singular present tense verb"
+          ), ncol = 3, byrow = TRUE, dimnames = list(c(), c("tag", "wclass", "desc"))),
+        tag.class.def.punct=matrix(c(
+        # tag           class           meaning
+          "pun",        "punctuation",  "Non-sentential punctuation", # (e.g. comma)
+          "punc",       "punctuation",  "punctuation"
+          ), ncol = 3, byrow = TRUE, dimnames = list(c(), c("tag", "wclass", "desc"))),
+        tag.class.def.sentc=matrix(c(
+        # tag           class           meaning
+          "$.",       "fullstop",     "Sentence-final punctuation"
+          ), ncol = 3, byrow = TRUE, dimnames = list(c(), c("tag", "wclass", "desc")))
+      )
+    )
+  )
+}
+
+# this internal, non-exported function causes the language support to be
+# properly added when the package gets loaded
+.onAttach <- function(...) {
+  lang.support.nl()
+}
