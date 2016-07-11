bce7b2ece15ab834b2f55678550be605ad8651fb
unDocUMeantIt <m@reaktanz.de>
2016-07-11 23:42:32 +0200

    updated release

--- R/koRpus.lang.nl-package.R
+++ R/koRpus.lang.nl-package.R
@@ -4,8 +4,8 @@
 #' Package: \tab koRpus.lang.nl\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.01-3\cr
-#' Date: \tab 2016-07-06\cr
-#' Depends: \tab R (>= 2.10.0),koRpus (>= 0.06-3), methods\cr
+#' Date: \tab 2016-07-11\cr
+#' Depends: \tab R (>= 2.10.0),koRpus (>= 0.07-1), methods\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>= 3)\cr
 #' LazyLoad: \tab yes\cr
